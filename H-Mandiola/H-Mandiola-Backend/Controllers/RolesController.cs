d9b5283d10fee0c090646ed10501f4190d0026f9
Andres Zhen <andreszhen@live.com>
2019-07-18 01:02:11 -0600

    Se crearon los controllers

---
+++ H-Mandiola/H-Mandiola-Backend/Controllers/RolesController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace H_Mandiola_Backend.Controllers
+{
+    public class RolesController
+    {
+    }
+}
