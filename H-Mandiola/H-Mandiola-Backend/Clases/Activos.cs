635c26496ec651c5e42d363cc63f07b74a80892a
LuisitoCrew19 <45248187+LuisitoCrew19@users.noreply.github.com>
2019-07-19 12:03:59 -0600

    Activos

--- H-Mandiola/H-Mandiola-Backend/Clases/Activos.cs
+++ H-Mandiola/H-Mandiola-Backend/Clases/Activos.cs
@@ -7,5 +7,10 @@ namespace H_Mandiola_Backend.Clases
 {
     public class Activos
     {
+        public string Prefijo { get; set; }
+        public string Descripcion { get; set; }
+        public string Codigo_Consecutivo { get; set; }
+        public string Nombre { get; set; }
+       
     }
 }
