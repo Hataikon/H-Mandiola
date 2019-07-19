f6bec72b75f9f0abb15203efb0d745b1200bb422
Yuan Man <yuan.897@gmail.com>
2019-07-19 02:20:41 -0600

    Consecutivo de Habitacion casi listo

--- H-Mandiola/H-Mandiola-Backend/Controllers/HabitacionesController.cs
+++ H-Mandiola/H-Mandiola-Backend/Controllers/HabitacionesController.cs
@@ -1,11 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BLL;
 
-namespace H_Mandiola_Backend.Controllers
+
+namespace H_Mandiola_Backend
 {
-    public class HabitacionesController
+    public class HabitacionesController : ApiController
     {
+        BLL_habitaciones objHabitaciones = new BLL_habitaciones();
+
+        [Route("~/api/Habitaciones")]
+        [HttpGet]
+        public IEnumerable<Habitaciones> GetAll()
+        {
+            DataSet ds = objHabitaciones.carga_lista_habitaciones();
+            DataTable dt = ds.Tables[0];
+
+            var retList = new List<Habitaciones>();
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                var row = dt.Rows[i];
+
+                var temp = new Habitaciones()
+                {
+                    Codigo_Consecutivo = Convert.ToString(row["CODIGO_CONSECUTIVO"]),
+                    Prefijo = Convert.ToString(row["PREFIJO"]),
+                    Numero = Convert.ToString(row["NUMERO"]),
+                    Descripcion = Convert.ToString(row["DESCRIPCION CONSECUTIVO"]),
+                    Imagen = Convert.ToString(row["IMAGEN_HABITACION"])
+                };
+
+                retList.Add(temp);
+            }
+
+            return retList;
+        }
+
+        [Route("~/api/Habitaciones/AgregarHabitacion")]
+        [HttpPost]
+        public IHttpActionResult AgregarHabitacion([FromBody]Habitaciones value)
+        {
+            objHabitaciones.codigo_consecutivo = value.Codigo_Consecutivo.ToString();
+            objHabitaciones.prefijo = value.Prefijo.ToString();
+            objHabitaciones.numero = value.Numero.ToString();
+            objHabitaciones.descripcion = value.Descripcion.ToString();
+            objHabitaciones.imagen_habitacion = value.Imagen.ToString();
+            if (objHabitaciones.agregar_habitacion())
+            {
+                return Json(new { msg = "Successfully added " + value.Prefijo.ToString() });
+            }
+            else
+            {
+                return Json(new { msg = "Error " + value.Prefijo.ToString() });
+            }
+            //String res = "Exito la wea wn qliao los valores son "+value.Prefijo+" "+value.CODIGO_CONSECUTIVO;
+
+        }
     }
+
 }
--- H-Mandiola/H-Mandiola-Backend/js/habitaciones.js
+++ H-Mandiola/H-Mandiola-Backend/js/habitaciones.js
@@ -6,4 +6,65 @@
 
     $("#usernameNavBar").text(getCookie("username"));
 
+    $.getJSON("/api/Consecutivos", function (data) {
+        console.log(data);
+        var filas = '';
+        data.forEach(function (fila) {
+            if (fila['Descripcion'] == "Habitacion") {
+                $('#consecutivoDropdown').append('<option value="' + fila['Prefijo'].trim() + "-" + fila["Codigo_Consecutivo"].trim() + '">' + fila['Prefijo'].trim() + "-" + fila["Codigo_Consecutivo"].trim() + '</option>');
+            }
+        })
+        $("#codigoBox").val($('#consecutivoDropdown').val());
+
+    });
+
+    $("#consecutivoDropdown").change(function (e) {
+        e.preventDefault();
+        $("#codigoBox").val($(this).children("option:selected").val());
+    });
+
+    $("#btnImagen").change(function (e) {
+        e.preventDefault();
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                base64 = e.target.result;
+                $('#imagen').attr('src', e.target.result).width(150).height(200);
+                $('#imageString').text(e.target.result);
+            };
+
+            reader.readAsDataURL(this.files[0])
+        };
+    });
+
+    $('#btnAceptar').click(function (e) {
+        e.preventDefault();
+        var Codigo_Consecutivo = $('#codigoBox').val();
+        var Prefijo;
+        var Numero;
+        var Descripcion;
+        var image = $('#imageString').text();
+        
+        console.log(image);
+        /*resJSON = JSON.stringify({ Username: Username, isAdmin: isAdmin, isSeguridad: isSeguridad, isConsecutivo: isConsecutivo, isMantenimiento: isMantenimiento, isConsulta: isConsulta });
+        alert(resJSON);
+        console.log(resJSON);
+        $.ajax({
+            type: "post",
+            url: "api/Usuarios/ActualizarRoles",
+            data: JSON.stringify({ Username: Username, isAdmin: isAdmin, isSeguridad: isSeguridad, isConsecutivo: isConsecutivo, isMantenimiento: isMantenimiento, isConsulta: isConsulta }),
+            dataType: "json",
+            contentType: "application/json",
+            success: function (response) {
+                console.log(response.msg)
+                alert(response.msg)
+            },
+            error: function (response) {
+                console.log(response);
+                window.location.replace("error.html?error=" + response.status + "&men=Error_Agregando_Consecutivo");
+            }
+        });*/
+    });
+
+
 });
