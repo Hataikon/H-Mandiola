e59e47beb177498a203cf08ef853912f4f2f4173
Andres Zhen <andreszhen@live.com>
2019-08-16 17:03:22 -0600

    Bll de clientes hecho, redireccion del login de google hecho, rregistro parcialmente hecho

--- H-Mandiola/BLL/BLL_cliente.cs
+++ H-Mandiola/BLL/BLL_cliente.cs
@@ -12,6 +12,7 @@ namespace BLL
 {
     public class BLL_cliente
     {
+        #region variables publicas
         private string _nombre_usuario;
         public string nombre_usuario
         {
@@ -49,6 +50,14 @@ namespace BLL
             set { _email = value; }
         }
 
+        private string _contrasena;
+
+        public string contrasena
+        {
+            get { return _contrasena; }
+            set { _contrasena = value; }
+        }
+
         private string _mensaje;
 
         public string mensaje
@@ -64,5 +73,123 @@ namespace BLL
             get { return _num_error; }
             set { _num_error = value; }
         }
+
+        #endregion
+
+        #region variables privadas
+        SqlConnection conexion;
+        string mensaje_error;
+        int numero_error;
+        string sql;
+        DataSet ds;
+        #endregion
+
+        #region metodos 
+
+        public bool agregar_cliente()
+        {
+            conexion = cls_DAL.trae_conexion("ConexionUusario", ref mensaje_error, ref numero_error);
+            if (conexion == null)
+            {
+                //insertar en la table de errores
+                HttpContext.Current.Server.Transfer("Error.aspx?error=" + numero_error.ToString() + "&men=" + mensaje_error);
+                cls_DAL.desconectar(conexion, ref mensaje_error, ref numero_error);
+                return false;
+            }
+            else
+            {
+                sql = "sp_AGREGAR_CLIENTE";
+                ParamStruct[] parametros = new ParamStruct[4];
+                cls_DAL.agregar_datos_estructura_parametros(ref parametros, 0, "@Nombre", SqlDbType.NVarChar, _nombre);
+                cls_DAL.agregar_datos_estructura_parametros(ref parametros, 1, "@Primerapellido", SqlDbType.NVarChar, _primer_apellido);
+                cls_DAL.agregar_datos_estructura_parametros(ref parametros, 2, "@Segundoapellido", SqlDbType.NVarChar, _segundo_apellido);
+                cls_DAL.agregar_datos_estructura_parametros(ref parametros, 3, "@Usuario", SqlDbType.NVarChar, _nombre_usuario);
+                cls_DAL.agregar_datos_estructura_parametros(ref parametros, 4, "@Email", SqlDbType.NVarChar, _email );
+                cls_DAL.agregar_datos_estructura_parametros(ref parametros, 5, "@Password", SqlDbType.NVarChar, _contrasena);
+
+                cls_DAL.conectar(conexion, ref mensaje_error, ref numero_error);
+                cls_DAL.ejecuta_sqlcommand(conexion, sql, true, parametros, ref mensaje_error, ref numero_error);
+
+                if (numero_error != 0)
+                {
+                    //insertar en la table de errores
+                    HttpContext.Current.Server.Transfer("Error.aspx?error=" + numero_error.ToString() + "&men=" + mensaje_error);
+                    cls_DAL.desconectar(conexion, ref mensaje_error, ref numero_error);
+                    return false;
+                }
+                else
+                {
+                    cls_DAL.desconectar(conexion, ref mensaje_error, ref numero_error);
+                    return true;
+                }
+            }
+        }
+
+        public bool actualizar_contrasena()
+        {
+            conexion = cls_DAL.trae_conexion("ConexionUusario", ref mensaje_error, ref numero_error);
+            if (conexion == null)
+            {
+                //insertar en la table de errores
+                HttpContext.Current.Server.Transfer("Error.aspx?error=" + numero_error.ToString() + "&men=" + mensaje_error);
+                cls_DAL.desconectar(conexion, ref mensaje_error, ref numero_error);
+                return false;
+            }
+            else
+            {
+                sql = "sp_cambiocontra";
+                ParamStruct[] parametros = new ParamStruct[2];
+                cls_DAL.agregar_datos_estructura_parametros(ref parametros, 0, "@Nombre", SqlDbType.NVarChar, _nombre);
+                cls_DAL.agregar_datos_estructura_parametros(ref parametros, 1, "@Contrasena", SqlDbType.NVarChar, _contrasena);
+
+                cls_DAL.conectar(conexion, ref mensaje_error, ref numero_error);
+                cls_DAL.ejecuta_sqlcommand(conexion, sql, true, parametros, ref mensaje_error, ref numero_error);
+
+                if (numero_error != 0)
+                {
+                    //insertar en la table de errores
+                    HttpContext.Current.Server.Transfer("Error.aspx?error=" + numero_error.ToString() + "&men=" + mensaje_error);
+                    cls_DAL.desconectar(conexion, ref mensaje_error, ref numero_error);
+                    return false;
+                }
+                else
+                {
+                    cls_DAL.desconectar(conexion, ref mensaje_error, ref numero_error);
+                    return true;
+                }
+            }
+        }
+
+
+        public DataSet lista()
+        {
+            conexion = cls_DAL.trae_conexion("ConexionUusario", ref mensaje_error, ref numero_error);
+            if (conexion == null)
+            {
+                //insertar en la table de errores
+                HttpContext.Current.Server.Transfer("Error.aspx?error=" + numero_error.ToString() + "&men=" + mensaje_error);
+                cls_DAL.desconectar(conexion, ref mensaje_error, ref numero_error);
+                return null;
+            }
+            else
+            {
+                sql = "sp_lista";
+                cls_DAL.conectar(conexion, ref mensaje_error, ref numero_error);
+                ds = cls_DAL.ejecuta_dataset(conexion, sql, true, ref mensaje_error, ref numero_error);
+                if (numero_error != 0)
+                {
+                    //insertar en la table de errores
+                    HttpContext.Current.Server.Transfer("Error.aspx?error=" + numero_error.ToString() + "&men=" + mensaje_error);
+                    return null;
+                }
+                else
+                {
+                    return ds;
+                }
+            }
+        }
+
+
+        #endregion
     }
 }
--- H-Mandiola/H-Mandiola-Frontend/js/login.js
+++ H-Mandiola/H-Mandiola-Frontend/js/login.js
@@ -72,6 +72,8 @@ function onSignIn(googleUser) {
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     var id_token = googleUser.getAuthResponse().id_token;
     console.log(id_token);
+    console.log("Redirecting...")
+    location.href = 'Reservaciones_Pagadas.html';
 }
 
 function signOut() {
